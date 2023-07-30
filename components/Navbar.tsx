71b4ac6dd5a9bd19e5b3b6baac9ebdab26e2e8d8
Mintfrog_1209 <hyb120934@gmail.com>
2023-07-31 05:40:40 +0900

    fix : image error re2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import NavItem from './NavItem';
+import Logo from '../public/lotus.png'
 
 const Navbar = () => {
   return (
     <div className="fixed w-full h-20 z-50 bg-[#B9F3FC]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <div>
-          <Image src='/../public/lotus.png' width='40' height='40' alt='/'/>
+          <Image src={Logo} width='40' height='40' alt='/'/>
         </div>
         <div>
           <ul className="flex">
